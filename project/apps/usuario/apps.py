550b7af362ca7db41778452e7298f600a0fd2688
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-17 23:56:08 -0300

    cambio de nombre en apps.py de usuario

--- project/apps/usuario/apps.py
+++ project/apps/usuario/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SuscriptionConfig(AppConfig):
+class UsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'suscription'
+    name = 'usuario'
