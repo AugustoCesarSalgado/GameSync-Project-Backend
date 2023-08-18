339aaa1b3c2c717e37c8a8fd9a8abaf71a003b41
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-17 23:52:56 -0300

    creando y registrando models de usuario

--- project/apps/usuario/admin.py
+++ project/apps/usuario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'correo', 'fecha_nacimiento')
+    list_filter = ('nombre', 'apellido', 'fecha_nacimiento')
+    search_fields = ('nombre', 'apellido', 'correo', 'fecha_nacimiento')
--- project/apps/usuario/models.py
+++ project/apps/usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50, null=False)
+    apellido = models.CharField(max_length=50, null=False)
+    correo = models.EmailField(null=False)
+    fecha_nacimiento = models.DateField(null=False)
+    def __str__(self):
+        return f'{self.nombre}, {self.apellido}, {self.correo}, {self.fecha_nacimiento}'
