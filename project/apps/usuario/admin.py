7d650c9e86b43970f3b38c09d27a69fabaff0898
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-18 00:26:25 -0300

    Borrando app Usuario

--- project/apps/usuario/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-@admin.register(models.Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'apellido', 'correo', 'fecha_nacimiento')
-    list_filter = ('nombre', 'apellido', 'fecha_nacimiento')
-    search_fields = ('nombre', 'apellido', 'correo', 'fecha_nacimiento')
--- project/apps/usuario/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    apellido = models.CharField(max_length=50, null=False)
-    correo = models.EmailField(null=False)
-    fecha_nacimiento = models.DateField(null=False)
-    def __str__(self):
-        return f'{self.nombre}, {self.apellido}, {self.correo}, {self.fecha_nacimiento}'
