bbfe3feb12daf274d82e352617953dd576b9cefa
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-19 15:54:09 -0300

    Registrando y models en app productos

--- project/apps/productos/admin.py
+++ project/apps/productos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from . import models
+
+admin.site.site_title = 'Productos'
+
+@admin.register(models.Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion',)
+    list_filter = ('nombre',)
+    search_fields = ('nombre', 'descripcion',)
+
+@admin.register(models.Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'cantidad', 'fecha_ingreso', 'descripcion', 'categoria',)
+    list_display_links = ('nombre',)
+    list_filter = ('nombre', 'precio', 'cantidad', 'fecha_ingreso', 'categoria',)
+    search_fields = ('nombre', 'precio', 'cantidad', 'fecha_ingreso', 'categoria',)
+    ordering = ('nombre', 'categoria', 'fecha_ingreso',)
