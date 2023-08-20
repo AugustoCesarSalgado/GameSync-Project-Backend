bee3899c2ca35742dc9ebe64f1541ba9ecd100eb
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-19 23:50:57 -0300

    Creando views de productos

--- project/apps/productos/views.py
+++ project/apps/productos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DetailView
 
-# Create your views here.
+from .models import Producto
+from django.http import HttpRequest, HttpResponse
+
+from . import models
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    productos_registrados = Producto.objects.all()
+    context = {'productos': productos_registrados}
+    return render(request, 'productos/index.html', context)
+
+
+
+
+ 
