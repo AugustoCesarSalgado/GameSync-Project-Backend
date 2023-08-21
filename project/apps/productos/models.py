de5913958e09177e21419da0902852690b110d63
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-08-21 02:03:16 -0300

    Correccion de error en models de productos

--- project/apps/productos/models.py
+++ project/apps/productos/models.py
@@ -12,9 +12,9 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50, null=False)
     precio = models.PositiveIntegerField(null=False)
     cantidad = models.PositiveBigIntegerField(null=False)
-    fecha_ingreso = models.DateField(default=timezone.now, null=False)
+    fecha_actualizacion = models.DateField(default=timezone.now, null=False)
     descripcion = models.CharField(max_length=250, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
 
     def __str__(self):
-        return f'{self.nombre} - ${self.precio}. Cantidad: {self.cantidad}. Categoria: {self.categoria}. Ingresado el {self.fecha_ingreso}'
+        return f'{self.nombre} - ${self.precio}. Cantidad: {self.cantidad}. Categoria: {self.categoria}. Ingresado el {self.fecha_actualizacion}'
