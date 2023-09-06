1f5bb3b8c20ebe965e293754a6c666760556ac6e
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-09-06 17:52:33 -0300

    Registrando app venta en settings

--- project/config/settings.py
+++ project/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Home',
     'productos',
+    'venta',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
