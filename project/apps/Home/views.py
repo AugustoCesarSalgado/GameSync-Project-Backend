aa8638557b7934f2069b252f67ad23bc666a662a
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-09-06 18:20:53 -0300

    Creacion de login y register en views de Home

--- project/apps/Home/views.py
+++ project/apps/Home/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpRequest, HttpResponse
+
+from . import forms
 
 def home(request):
     return render(request, 'Home/index.html')
+
+# Login
+
+def login_request(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = forms.CustomAtuhenticationForm(request, request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            contraseña = form.cleaned_data.get('password')
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, 'Home/index.html', {'mensaje': 'Has iniciado sesión correctamente'})
+        else:
+            form = forms.CustomAtuhenticationForm()
+        return render(request, 'Home/login.html', {'form': form})
+    
+
+#Registro
+
+@staff_member_required
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = forms.CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request, "Home/index.html", {"mensaje": "Vendedor creado 😊"})
+    else:
+        form = forms.CustomUserCreationForm()
+    return render(request, "Home/register.html", {"form": form})
+
+
+
